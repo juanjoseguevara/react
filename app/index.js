5f58aafec2b31a400911a15a8e031e9ebcc2732f
juanjoseguevara <juanjose.99.alvarez@gmail.com>
2023-05-29 00:40:54 +0200

    test

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo! ¡Hola, mundo!');
+  res.send('¡TEST!');
 });
 
 app.listen(3000, () => {
